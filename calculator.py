6de84db0cca1b8f88df2d8996fdfd364beaacccc
Wendel <wchristofer@gmail.com>
2022-09-04 21:08:27 -0400

    inserting the while looping

--- calculator.py
+++ calculator.py
@@ -36,26 +36,27 @@ print("E. Exit")
 
 choice = input("input your choice: ")
 
-if choice == "a" or choice == "A":
-    print("Addtion")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    add(a, b)
-elif choice == "b" or choice == "B":
-    print("Subtraction")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    sub(a, b)
-elif choice == "c" or choice == "C":
-    print("Multiplication")
-    a = int(input("input first number: "))  
-    b = int(input("input second number: "))
-    mul(a, b)
-elif choice == "d" or choice == "D":
-    print("Division")
-    a = int(input("input first number: "))
-    b = int(input("input second number: "))
-    div(a, b)
-elif choice == "e" or choice == "E":
-    print("program ended")
-    quit()
+while True:                                     # this while will show the options till the user choice Exit
+    if choice == "a" or choice == "A":
+        print("Addtion")
+        a = int(input("input first number: "))
+        b = int(input("input second number: "))
+        add(a, b)
+    elif choice == "b" or choice == "B":
+        print("Subtraction")
+        a = int(input("input first number: "))
+        b = int(input("input second number: "))
+        sub(a, b)
+    elif choice == "c" or choice == "C":
+        print("Multiplication")
+        a = int(input("input first number: "))  
+        b = int(input("input second number: "))
+        mul(a, b)
+    elif choice == "d" or choice == "D":
+        print("Division")
+        a = int(input("input first number: "))
+        b = int(input("input second number: "))
+        div(a, b)
+    elif choice == "e" or choice == "E":
+        print("program ended")
+        quit()
