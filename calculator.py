1effd6d86bcfedb949b1cda6133c56786e280418
Wendel <wchristofer@gmail.com>
2022-09-04 21:11:40 -0400

    creating break lines for a better visualization

--- calculator.py
+++ calculator.py
@@ -8,23 +8,23 @@ from multiprocessing.connection import answer_challenge
 
 def add(a, b):
     answer = a + b
-    print(str(a) + " + " + str(b) + " = " + str(answer))
+    print(str(a) + " + " + str(b) + " = " + str(answer) + "\n")
     #the result will show the values of a and b and the value of the add
 
 def sub(a, b):
     answer = a - b
-    print(str(a) + " - " + str(b) + " = " + str(answer))
+    print(str(a) + " - " + str(b) + " = " + str(answer) + "\n")
     #the result will show the values of a and b and the value of the sub
 
 def mul(a, b):
     answer = a * b
-    print(str(a) + " * " + str(b) + " = " + str(answer))
+    print(str(a) + " * " + str(b) + " = " + str(answer) + "\n")
     #the result will show the values of a and b and the value of the mul
 
 
 def div(a, b):
     answer = a / b
-    print(str(a) + " / " + str(b) + " = " + str(answer))
+    print(str(a) + " / " + str(b) + " = " + str(answer) + "\n")
     #the result will show the values of a and b and the value of the div
 
 
