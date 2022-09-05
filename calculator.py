ab439f37b8604064c1db5bbc77132cc40a770796
Wendel <wchristofer@gmail.com>
2022-09-04 20:39:14 -0400

    creating add function

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# functions: add, sub, mul   and div
+# print options for the users
+# ask for values
+# call the functions
+
+from multiprocessing.connection import answer_challenge
+
+
+def add(a, b):
+    answer = a + b
+    print(str(a) + " + " + str(b) + " = " + str(answer))
+    #the result will show the values of a and b and the value of the add
+
+
+
